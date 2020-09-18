e2e7d11c3ea95fb1012a132f0ef7c9e3cca243b9
Mika Fonsén <69679012+no-mika@users.noreply.github.com>
2020-09-18 14:51:07 +0300

    Points, models and paging support (#2)
    New: models, points, paging. Includes fine tuning

--- README.md
+++ README.md
@@ -18,3 +18,87 @@ Example code showing how Manage external access can be used.
 2. Open `http://localhost:3000` (or different port, if so defined in `.env`)
 3. Complete Xsite® MANAGE login flow
 4. Observe refresh and id tokens returned in client stdout
+
+
+## API Usage
+
+### Common parameters
+For OAUTH2 based authorization, please see example code. Cors support is not available at the moment.
+
+### Common errors
+- 400 Bad request. Some request parameter is invalid
+- 401 Unauthorized. Token valid, but no access to requested resource
+- 403 Forbidden. Token is probably not valid
+
+Application error response body: `schema/common-error-body.json`. Please note that systems in between the application and the client also produce error responses of different format.
+
+### Paging
+Api endpoints thay may return large set of results may implement paging. In
+case there are too many results to fit in a single response, such api includes
+field `nextToken` in its response. The next page is fetched by sending
+the received value back in a field with the same name `nextToken`.
+
+Note that any endpoint listed here are supporting paging may change its maximum
+page size in the future. Some apis are initially implemented with huge page
+size (meaning no paging in effect), but it may be decreased in the future.
+
+## API endpoints
+
+#### GET https://api.xsitemanage.com/ext/0/site/sites?nextToken={nextToken}&maxPageSize={maxPageSize}
+Get user's sites
+
+Parameters
+- nextToken: Optional. For paging. Only used if given by an API response
+- maxPageSize: Optional. For paging. A page will not contain more items that the given value
+
+Response: `schema/sites-get-response.json`
+
+#### PUT https://api.xsitemanage.com/ext/0/site/protection
+Configure site directory protection. This should be set right after setting up the sync and creating the folder to which sync the data.
+This should be unset before disconnecting the sync
+
+Parameters
+- siteId: site id (string, uuid)
+- protection: Protection configuration like `{ "prefixes": { "protectedFolder/": "myCompany" } }`
+
+Response: `schema/protection-put-response.json`
+
+#### GET https://api.xsitemanage.com/ext/0/point/points?siteId={siteId}&since={sequenceId}&nextToken={nextToken}&maxPageSize={maxPageSize}
+Get log points for a site
+
+Parameters
+- siteId: site id (string, uuid)
+- since: Optional. List items after sequenceId. Excludes given sequenceId from the list
+- nextToken: Optional. For paging. Only used if given by an API response
+- maxPageSize: Optional. For paging. A page will not contain more items that the given value
+
+Response: `schema/points-get-response.json`
+
+#### GET https://api.xsitemanage.com/ext/0/model/latest?siteId={siteId}&nextToken={nextToken}&maxPageSize={maxPageSize}
+Get site files
+
+Parameters
+- siteId: site id (string, uuid)
+- nextToken: Optional. For paging. Only used if given by an API response
+- maxPageSize: Optional. For paging. A page will not contain more items that the given value
+
+Response: `schema/model-get-response.json`
+
+#### GET https://api.xsitemanage.com/ext/0/model/presign/file?siteId={siteId}&path={path}
+Presign path in preparation of adding it to a site. May be a new path or update of existing one.
+
+Parameters:
+- siteId: site id (string, uuid)
+- path: path
+
+Response: `schema/presign-get-response.json`
+
+#### POST https://api.xsitemanage.com/ext/0/model/command/add/file
+Add file in temporary location to site model hierarchy
+
+Parameters:
+- siteId: site id (string, uuid)
+- path: path of added file
+- presignRequestId: Request id returned from preceding call to `model/presign/file`
+
+Response: Empty object
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fastify = require("fastify")()
 const axios = require("axios").default
+const htmlencode = require("node-htmlencode")
 const { stringify } = require("querystring")
 require("dotenv").config()
 
@@ -8,12 +9,21 @@ const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
 const AUTH_DOMAIN = process.env.AUTH_DOMAIN
-const MANAGE_API_URL = process.env.MANAGE_API_URL
+const MANAGE_API_DOMAIN = process.env.MANAGE_API_DOMAIN
 // CSRF token should not be static in production use
 const CSRF_STATE = "REPLACE_WITH_CROSS_SITE_REQUEST_FORGERY_TOKEN"
 
 // Received id token
 let idToken
+
+/**
+ * Convert given json object for priting in html.
+ * @param {*} object Json object
+ * @returns html
+ */
+function htmlEncode(object) {
+  return htmlencode.htmlEncode(JSON.stringify(object))
+}
 
 /**
  * Base64 encode client id and secret for basic authentication
@@ -116,15 +126,290 @@ fastify.get("/callback", async (request, reply) => {
 // 3. Notify user that authorization was successful
 // 4. Do an example Manage api call with received token
 fastify.get("/success", async (request, reply) => {
-  const callUrl = `https://${MANAGE_API_URL}/site/sites?foo=bar`
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>Authentication</h1>
+      <h2>Actions</h2>
+      <a href="list">List sites</a>
+      <h2>Result</h2>
+      Authentication successful
+    </body></html>`
+})
+
+fastify.get("/list", async (request, reply) => {
   let response
+  let callUrl
+  const maxPageSize = request.query.maxPageSize || 5
+  const nextToken = request.query.nextToken
 
   try {
     const axiosConfig = {
       headers: { Authorization: idToken },
       // Never throw, we simply want to print the response
-      validateStatus: (status) => true,
+      validateStatus: () => true,
     }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/site/sites?maxPageSize=${maxPageSize}`
+    if (nextToken) callUrl += `&nextToken=${nextToken}`
+
+    response = await axios.get(callUrl, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  let siteTableRows = ""
+  for (const site of response.data.items) {
+    siteTableRows += `<tr>
+      <td>${site.siteId}</td>
+      <td>${site.name}</td>
+      <td><a href="points?siteId=${site.siteId}">List points</a></td>
+      <td><a href="files?siteId=${site.siteId}">List files</a></td>
+      <td><a href="protection?siteId=${site.siteId}">Enable for protectedFolder/ as companyName</a></td>
+    </tr>`
+  }
+
+  let nextPage = ""
+  if (response.data.nextToken)
+    nextPage = `<br>
+  <a href="list?nextToken=${response.data.nextToken}&maxPageSize=${maxPageSize}">Next sites</a>`
+
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>List sites</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>GET</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt>
+      <h2>Received sites</h2>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>List points</th>
+          <th>List files</th>
+          <th>Directory protection</th>
+        </tr>
+        ${siteTableRows}
+      </table>
+      ${nextPage}
+      <h2>Actions</h2>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${htmlEncode(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/points", async (request, reply) => {
+  const siteId = request.query.siteId
+  const nextToken = request.query.nextToken
+  const maxPageSize = request.query.maxPageSize || 5
+  let response
+  let callUrl
+
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/point/points?siteId=${siteId}`
+    if (request.query.nextToken) callUrl += `&nextToken=${nextToken}`
+    callUrl += `&maxPageSize=${maxPageSize}`
+    response = await axios.get(callUrl, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  const points = response.data.items
+  const sequenceIds = points.map((p) => p.sequenceId)
+  const minSequenceId = Math.min(...sequenceIds)
+  const maxSequenceId = Math.max(...sequenceIds)
+  let nextPage = ""
+  if (response.data.nextToken)
+    nextPage = `<a href="points?siteId=${siteId}&nextToken=${response.data.nextToken}&maxPageSize=${maxPageSize}">Get next page</a>`
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>List logpoints of site ${siteId}</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>GET</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt>
+      <h2>Received points</h2>
+      <b>Count:</b> ${points.length}<br>
+      <b>Min sequence id:</b> ${minSequenceId}<br>
+      <b>Max sequence id:</b> ${maxSequenceId}<br>
+      ${nextPage}
+      <h2>Actions</h2>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${htmlEncode(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/files", async (request, reply) => {
+  const siteId = request.query.siteId
+  const maxPageSize = request.query.maxPageSize || 5
+  const nextToken = request.query.nextToken
+  let response
+  let callUrl
+
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/model/latest?siteId=${siteId}`
+    if (maxPageSize) callUrl = callUrl + `&maxPageSize=${maxPageSize}`
+    if (nextToken) callUrl = callUrl + `&nextToken=${nextToken}`
+    response = await axios.get(callUrl, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  const files = response.data.items
+  const totalSize = files.reduce((total, file) => total + (file.size || 0), 0)
+
+  let fileTableRows = ""
+  for (const file of files) {
+    const encodedUrl = encodeURIComponent(file.downloadUrl)
+    const path = file.path
+    fileTableRows += `<tr>
+      <td>${file.path}</td>
+      <td>${file.type || ""}</td>
+      <td>${file.size}</td>
+      <td>${file.version}</td>
+      <td>${new Date(file.timestampMs).toISOString()}</td>
+      <td><a href="/download?url=${encodedUrl}">Download</a></td>
+      <td><a href="/presign?siteId=${siteId}&path=${path}">Upload new version</a></td>
+    </tr>`
+  }
+
+  const newPath = Date.now()
+  let nextPage = ""
+
+  if (response.data.nextToken)
+    nextPage = `<br>
+    <a href="files?siteId=${siteId}&nextToken=${response.data.nextToken}&maxPageSize=${maxPageSize}">Get next page</a>`
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>List files of site ${siteId}</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>GET</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt>
+      <h2>Received files</h2>
+      <b>Site version:</b> ${response.data.siteVersion}<br>
+      <b>Count:</b> ${files.length}<br>
+      <b>Total size:</b> ${totalSize}<br>
+      <table>
+        <tr>
+          <th>Path</th>
+          <th>Type</th>
+          <th>Size</th>
+          <th>Version</th>
+          <th>Timestamp</th>
+          <th>Download</th>
+          <th>Upload new version</th>
+        </tr>
+        ${fileTableRows}
+      </table>
+      ${nextPage}
+      <h2>Actions</h2>
+      <a href="presign?siteId=${siteId}&path=${newPath}">Upload new file with name ${newPath}</a><br>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${JSON.stringify(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/protection", async (request, reply) => {
+  const siteId = request.query.siteId
+
+  let response
+  let callUrl
+  let callBody
+
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/site/protection`
+    callBody = {
+      siteId,
+      protection: {
+        prefixes: { "protectedFolder/": "companyName" },
+      },
+    }
+    response = await axios.put(callUrl, callBody, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>Set directory protection for site ${siteId}</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>PUT</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt><br>
+      <b>Request body:</b> <tt>${JSON.stringify(callBody)}</tt>
+      <h2>Actions</h2>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${JSON.stringify(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/download", async (request, reply) => {
+  const callUrl = request.query.url
+
+  let response
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
     response = await axios.get(callUrl, axiosConfig)
 
     const dataStr = JSON.stringify(response.data)
@@ -140,12 +425,145 @@ fastify.get("/success", async (request, reply) => {
   reply.type("text/html")
   return `
     <html><body>
-      <h1>Log</h1>
-      <h2>Authentication</h2>
-      Authentication successful
+      <h1>Downloaded file at ${decodeURIComponent(callUrl)}</h1>
       <h2>Request</h2>
       <b>Method:</b> <tt>GET</tt><br>
-      <b>Url:</b> <tt>${callUrl}</tt>
+      <b>Url:</b> <tt>${callUrl}</tt><br>
+      <h2>Actions</h2>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${response.data}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/presign", async (request, reply) => {
+  const { siteId, path } = request.query
+
+  let callUrl
+
+  let response
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/model/presign/file?siteId=${siteId}&path=${path}`
+    response = await axios.get(callUrl, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  const { url, requestId: presignRequestId } = response.data
+
+  const encodedUrl = encodeURIComponent(url)
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>Presigning path ${path} of site ${siteId}</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>GET</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt><br>
+      <h2>Actions</h2>
+      <a href="upload?siteId=${siteId}&path=${path}&presignRequestId=${presignRequestId}&uploadUrl=${encodedUrl}">Upload file with contents <tt>test1\ntest2\n</tt></a><br>
+      <a href="list">List sites</a>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${htmlEncode(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/upload", async (request, reply) => {
+  const { siteId, path, presignRequestId, uploadUrl: callUrl } = request.query
+
+  let callBody
+  let response
+  try {
+    const axiosConfig = {
+      // No id token here, it is passed in url query parameters when uploading!
+      headers: { "Content-Type": "application/octet-stream" },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callBody = "test1\ntest2\n"
+    response = await axios.put(callUrl, callBody, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>Uploading path ${path} of site ${siteId} as temporary file</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>POST</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt><br>
+      <b>Request body:</b> <tt>${htmlEncode(callBody)}</tt>
+      <h2>Actions</h2>
+      <a href="addfile?siteId=${siteId}&path=${path}&presignRequestId=${presignRequestId}">Add uploaded file to site</a><br>
+      <a href="list">List sites</a><br>
+      <h2>Response</h2>
+      <b>Response http status code</b>: <tt>${response.status}</tt><br>
+      <b>Response data</b>: <tt>${htmlEncode(response.data)}</tt><br>
+    </body></html>`
+})
+
+fastify.get("/addfile", async (request, reply) => {
+  const { siteId, path, presignRequestId } = request.query
+
+  let callUrl
+  let callBody
+
+  let response
+  try {
+    const axiosConfig = {
+      headers: { Authorization: idToken },
+      // Never throw, we simply want to print the response
+      validateStatus: () => true,
+    }
+
+    callUrl = `https://${MANAGE_API_DOMAIN}/ext/0/model/command/add/file`
+    callBody = { siteId, path, presignRequestId }
+    response = await axios.post(callUrl, callBody, axiosConfig)
+
+    const dataStr = JSON.stringify(response.data)
+    console.log(
+      `Manage sites call response: Http status code: ${response.status}, Data: ${dataStr}`
+    )
+  } catch (err) {
+    const msg = `Error during Manage api call: ${err}`
+    console.log(msg)
+    throw new Error(msg)
+  }
+
+  reply.type("text/html")
+  return `
+    <html><body>
+      <h1>Adding previously added temporary path ${path} to site ${siteId}</h1>
+      <h2>Request</h2>
+      <b>Method:</b> <tt>POST</tt><br>
+      <b>Url:</b> <tt>${callUrl}</tt><br>
+      <b>Request body:</b> <tt>${htmlEncode(callBody)}</tt>
+      <h2>Actions</h2>
+      <a href="files?siteId=${siteId}">List site files</a><br>
+      <a href="list">List sites</a><br>
       <h2>Response</h2>
       <b>Response http status code</b>: <tt>${response.status}</tt><br>
       <b>Response data</b>: <tt>${JSON.stringify(response.data)}</tt><br>
